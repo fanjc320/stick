0e0085d03ecba64b74f119edb0dc843a9b3d3d8a
jackyfan <894020798@qq.com>
2023-11-11 17:11:14 +0800

    test ci

---
+++ old/火柴人/火柴人/huochairen/Assets/food.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class food : MonoBehaviour {
+
+    public static Action action;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (action != null)
+            {
+                action();
+            }
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+    }
+}
